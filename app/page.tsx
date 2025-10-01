ea8620ba263645a7c6ea8dc375dc1f5e38d538f1
ar.mangir@tetracode.dev <ar.mangir@tetracode.dev>
2025-10-01 18:24:43 +0300

    first commit v2

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,76 @@
-import Link from 'next/link';
-import { Title } from './shared/components/title/Title';
+import Link from "next/link";
+import { Title } from "./shared/components/title/Title";
+import s from "./shared/components/cards/mainSection.module.css";
+
+const siteData = [
+  {
+    id: 0,
+    title: "Mobile APP(PWA)",
+    open: true,
+    linkSite: "https://test.3-tone.ru/",
+  },
+  {
+    id: 1,
+    title: "Погода",
+    open: true,
+    gitLink: "https://github.com/magarteem/weather",
+    linkSite: "https://weather.jsx.su/",
+  },
+  {
+    id: 2,
+    title: "Чай",
+    open: true,
+    skilsHtml: [],
+    gitLink: "https://github.com/magarteem/chai",
+    linkSite: "https://chai.jsx.su/",
+  },
+  {
+    id: 3,
+    title: "Авиа билеты",
+    open: true,
+    gitLink: "https://github.com/magarteem/avia_sales",
+    linkSite: "https://air.jsx.su/",
+  },
+  {
+    id: 4,
+    title: "Кроссовки",
+    open: true,
+    gitLink: "https://github.com/magarteem/sneakers",
+    linkSite: "https://sneacers.jsx.su/",
+  },
+];
 
 export default function CodeExample() {
   return (
     <main className="flex min-h-screen flex-col  p-4">
+      {siteData.map((e) => (
+        <div key={e.id} className={s.cardItemMain}>
+          <div
+            className={
+              s.showInfoBefore + " " + `${e.open && s.transformTranslate}`
+            }
+          >
+            <div className={s.link}>
+              {e.gitLink && (
+                <a href={e.gitLink} target="_blank">
+                  uuuuuuuuuu
+                </a>
+              )}
+              <a href={e.linkSite} target="_blank">
+                tttttttttttt
+              </a>
+            </div>
+
+            <div className={s.__open}>uuuuuuuuuuu</div>
+          </div>
+          <div className={s.showInfoAfter}>
+            <div className={s._header}>
+              <a>{e.title}</a>
+            </div>
+          </div>
+        </div>
+      ))}
+
       <Link href="base">
         <Title title="Solidity. Базовый уровень" />
       </Link>
